3820d211b1168e7a29c7db181544d16ac5ab7b24
Prasanna <prasannaupatankar@gmail.com>
2023-03-08 12:51:06 +0530

    Updated Employee & Company service code

--- GRPCClient/Program.cs
+++ GRPCClient/Program.cs
@@ -16,7 +16,7 @@ namespace GRPCClient
             {
                 await postComapny();
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 Console.WriteLine(ex.Message);
             }
@@ -26,7 +26,7 @@ namespace GRPCClient
             try
             {
 
-                Console.WriteLine("press enter to call the Gprc service...");
+                Console.WriteLine("Press enter to initiate the Gprc service...");
                 Console.ReadLine();
                 //Update the URL with your local or remote URL
                 var channel = GrpcChannel.ForAddress("https://localhost:5001", new GrpcChannelOptions
@@ -34,36 +34,65 @@ namespace GRPCClient
                     HttpHandler = new GrpcWebHandler(new HttpClientHandler())
                 });
                 var readerClient = new Company.CompanyClient(channel);
-
-                var emps = new EmployeeModel[]
+                //Creating new employees
+                var employees = new EmployeeModel[]
                 {
-                new EmployeeModel
-                {
-                     EmpId = 1,
-                     EmpName = "John Doe",
-                     BirthDate = Timestamp.FromDateTime(DateTime.UtcNow),
-                     CompanyId = 1
-                },
-                new EmployeeModel
-                {
-                     EmpId = 2,
-                     EmpName = "Mark Lue",
-                     BirthDate = Timestamp.FromDateTime(DateTime.UtcNow),
-                     CompanyId = 1
-                }
+                    new EmployeeModel
+                    {
+                        EmpId = 1,
+                        EmpName = "Rama Bapat",
+                        BirthDate = Timestamp.FromDateTime(DateTime.UtcNow.AddYears(-24).AddMonths(-4)),
+                        CompanyId = 1
+                    },
+                    new EmployeeModel
+                    {
+                        EmpId = 2,
+                        EmpName = "Krishna Nene",
+                        BirthDate = Timestamp.FromDateTime(DateTime.UtcNow.AddYears(-22).AddMonths(-7)),
+                        CompanyId = 1
+                    }
                 };
-
-                var compData = new CompanyModel
+                //Creating new company
+                var company = new CompanyModel
                 {
                     CompanyId = 1,
-                    ComapnyName = "GRPC Company"
+                    ComapnyName = "Patankar Khauwale"
                 };
-                compData.Employees.Add(emps);
-                var responseData = await readerClient.PostCompanyWithEmployeesAsync(compData);
-                Console.WriteLine($"Status : {responseData.Status}");
+
+                //Printing the Company & Employee details
+                //This part can be removed, nothing to do with the functionality
+                Console.WriteLine("----------Employee details----------");                
+                foreach (EmployeeModel emp in employees)
+                {
+                    Console.WriteLine("Id : " + emp.EmpId);
+                    Console.WriteLine("Name : " + emp.EmpName);
+                    Console.WriteLine("Birthdate : " + emp.BirthDate);
+                    Console.WriteLine("CompanyId : " + emp.CompanyId);
+                    Console.WriteLine("----------");
+                }
+                Console.WriteLine("----------Company details----------");                
+                Console.WriteLine("Id : " + company.CompanyId);
+                Console.WriteLine("Name : " + company.ComapnyName);
+                Console.WriteLine("Press enter to call the Gprc service...");
+                Console.ReadLine();
+
+                //Adding employees to company model
+                company.Employees.Add(employees);
+                //Calling the server channel
+                var responseData = await readerClient.PostCompanyWithEmployeesAsync(company);
+
+                if (responseData.Status == 1)
+                { 
+                    Console.WriteLine("Company & Employees added Successfully.");
+                }
+                else 
+                {
+                    Console.WriteLine("Company & Employees could not be added, please try again.");
+                }
+
                 Console.ReadLine();
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 throw ex;
             }
